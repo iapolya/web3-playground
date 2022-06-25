fc92d044c53b555845c7570adddb6f6f29a78713
iapolya <polina7alikina@gmail.com>
2022-06-25 22:16:40 +0300

    + task example

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,16 +1,7 @@
 require('@nomiclabs/hardhat-waffle')
 require('dotenv').config()
 require('@nomiclabs/hardhat-etherscan') // in ts import '@nomiclabs/hardhat-etherscan'
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners()
-
-  for (const account of accounts) {
-    console.log(account.address)
-  }
-})
+require('./tasks/blockNumber')
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
