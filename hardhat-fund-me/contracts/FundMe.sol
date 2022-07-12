a02f7ebb2c9e409370c5ae2b05f00c26b5996832
iapolya <polina7alikina@gmail.com>
2022-07-12 23:45:51 +0300

    + fund me tests

--- hardhat-fund-me/contracts/FundMe.sol
+++ hardhat-fund-me/contracts/FundMe.sol
@@ -1,25 +1,47 @@
 // SPDX-License-Identifier: UNLICENSED
+// 1. Pragms
 pragma solidity ^0.8.9;
 
+// 2. Imports
 import './PriceConverter.sol';
 
-error NotOwner();
+// 3. Error codes
+error FundMe__NotOwner();
+
+// Interfaces, Libraries, Contracts
 
 contract FundMe {
+    // 1. type declarations
     using PriceConverter for uint256;
 
+    // 2. State variables
     uint256 public constant MIN_USD = 10 * 1e18;
-
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
-
     address public immutable i_owner;
-
     AggregatorV3Interface public priceFeed;
+
+    // 3. Events
+
+    // 4. Modifiers
+    modifier olnyOwner() {
+        if (msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
+        _;
+    }
 
     constructor(address priceFeedAddress) {
         i_owner = msg.sender;
         priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 
     function fund() public payable {
@@ -41,20 +63,5 @@ contract FundMe {
             value: address(this).balance
         }('');
         require(callSuccess, 'Call failed((((');
-    }
-
-    modifier olnyOwner() {
-        if (msg.sender != i_owner) {
-            revert NotOwner();
-        }
-        _;
-    }
-
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
     }
 }
---
+++ hardhat-fund-me/test/unit/FundMe.test.ts
@@ -0,0 +1,80 @@
+import { assert, expect } from 'chai'
+import { Contract } from 'ethers'
+import { deployments, ethers, getNamedAccounts } from 'hardhat'
+import { Address } from 'hardhat-deploy/dist/types'
+import { MockV3Aggregator } from '../../typechain-types'
+
+describe('FundMe', async function () {
+  let fundMe: Contract
+  let deployer: Address
+  let mockV3Aggregator: MockV3Aggregator
+  const sendValue = ethers.utils.parseEther('1')
+
+  beforeEach(async function () {
+    // в hardhat network просто доступные аккаунты
+    // const accounts = await ethers.getSigners()
+    // const account1 = accounts[0]
+
+    deployer = (await getNamedAccounts()).deployer
+
+    // вызов скриптов из /deploy с тегами
+    await deployments.fixture(['all'])
+
+    fundMe = await ethers.getContract('FundMe', deployer)
+    mockV3Aggregator = await ethers.getContract('MockV3Aggregator', deployer)
+  })
+
+  describe('constructor', async function () {
+    it('check mock address', async function () {
+      const response = await fundMe.priceFeed()
+      assert.equal(response, mockV3Aggregator.address)
+    })
+  })
+
+  describe('fund', async function () {
+    it('fails if there is no ETH', async function () {
+      await expect(fundMe.fund()).to.be.revertedWith('Didnt send ehough')
+    })
+
+    it('send some ETH', async function () {
+      await fundMe.fund({ value: sendValue })
+      const response = await fundMe.addressToAmountFunded(deployer)
+      assert.equal(response.toString(), sendValue.toString())
+    })
+
+    it('add funder', async function () {
+      await fundMe.fund({ value: sendValue })
+      const response = await fundMe.funders(0)
+      assert.equal(response, deployer)
+    })
+  })
+
+  describe('withdraw', async function () {
+    beforeEach(async function () {
+      await fundMe.fund({ value: sendValue })
+    })
+
+    it('check balance after withdraw', async function () {
+      const startingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      )
+      const startingDeployerBalance = await fundMe.provider.getBalance(deployer)
+
+      const transactionResponse = await fundMe.withdraw()
+      const transactionReceipt = await transactionResponse.wait()
+      const { gasUsed, effectiveGasPrice } = transactionReceipt
+      const gasCost = gasUsed.mul(effectiveGasPrice)
+
+      const endingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      )
+      const endingDeployerBalance = await fundMe.provider.getBalance(deployer)
+
+      assert.equal(endingFundMeBalance.toString(), '0')
+      assert.equal(
+        startingFundMeBalance.add(startingDeployerBalance).toString(),
+        endingDeployerBalance.add(gasCost).toString()
+      )
+    })
+  })
+})
